5e940de5b96c2316b253122d6379c944c8ea41db
Ruby Lee <rubymleee@gmail.com>
2018-09-21 18:47:41 -0700

    sidebar and peoplealsobought working

--- proxy/server.js
+++ proxy/server.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const parser = require('body-parser');
+const logger = require('morgan');
 // const proxy = require('express-http-proxy');
 var proxy = require('http-proxy-middleware');
-// const app = express();
+const app = express();
 const port = process.env.PORT || 3000;
 // app.use('/api', proxy({target: 'http://www.example.org', changeOrigin: true}));
 // app.listen(3000);
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.use(morgan('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/:company', express.static(path.join(__dirname, 'public')));
-app.use('/api', proxy('localhost:3001'));
-app.use('/data/company', proxy('localhost:3002'));
-app.use('people-also-bought', proxy('localhost:3003'));
-app.use('stocks/sideBar', proxy('localhost:3004'));
+
+app.use('/api/graph/', proxy({target:'http://localhost:3001'}));
+app.use('/api/data/company/', proxy({target:'http://localhost:3002'}));
+app.use('/api/people-also-bought/', proxy({target:'http://localhost:3003'}));
+app.use('/api/sideBar/', proxy({target:'http://localhost:3004'}));
 
 
 app.listen(port, () => {
